612d551b411ebb53c45c5bb5dbb13e13f0b3865b
parabshubham787 <parabshubham787@gmail.com>
2021-09-17 10:14:13 +0530

    changes successfully

--- Welcome/src/com/csi/core/CoreJava.java
+++ Welcome/src/com/csi/core/CoreJava.java
@@ -1,9 +1,9 @@
 package com.csi.core;
-
+//Added by Shubham Parab
 public class CoreJava 
 {
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to CSI Pune");
+		System.out.println("Welcome to Full Stack Java Developer CSI Pune");
 	}
 }
