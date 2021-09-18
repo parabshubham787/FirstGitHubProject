44ed867ba1bbd6ad3c4376c8940aa7814120e7b7
parabshubham787 <parabshubham787@gmail.com>
2021-09-18 10:52:48 +0530

    Changes made again

--- Welcome/src/com/csi/core/CoreJava.java
+++ Welcome/src/com/csi/core/CoreJava.java
@@ -2,8 +2,14 @@ package com.csi.core;
 //Added by Shubham Parab
 public class CoreJava 
 {
+	int EmpId = 1111;
+	static String Ename = "Shubham";
+			
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Full Stack Java Developer CSI Pune");
+		CoreJava c1 = new CoreJava();
+		System.out.println("\n Welcome to Full Stack Java Developer CSI Pune");
+		System.out.println("\n Employee Name = " + Ename);
+		System.out.println("\n Employee ID = "+ c1.EmpId);
 	}
 }
